557e5252264e80b1703dad243e3892546002cf2d
King Grey <kinggrey9959@gmail.com>
2024-03-31 15:26:53 +0530

    Cleaned

--- src/screens/Authentication/Welcome.jsx
+++ src/screens/Authentication/Welcome.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { useTheme } from 'react-native-paper';
 
 const Welcome = () => {
+  const theme = useTheme();
   //  const handleSignIn = () => {
-  //   // Perform your sign-in logic here
-  //   // For example, after successful sign-in:
   //   const user = { id: 1, username: 'example' }; // Replace with actual user data
   //   dispatch(loginSuccess(user));
   // };
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ color: 'black' }}>Welcome</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: theme.colors.background,
+      }}
+    >
+      <Text>Welcome</Text>
     </View>
   );
 };
