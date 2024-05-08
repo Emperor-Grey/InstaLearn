674a3d48428c4ca41e0841368603b67f55c81ccc
King Grey <kinggrey9959@gmail.com>
2024-05-08 20:47:47 +0530

    Testing build

--- App.js
+++ App.js
@@ -10,39 +10,39 @@ import {
   useSelector,
 } from 'react-redux/dist/react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import MainNav from './navigation/MainNav';
-import { selectCurrentTheme } from './redux/reducers/themeSlice';
-import store, { persister } from './redux/store';
+import MainNav from './src/navigation/MainNav';
+import { selectCurrentTheme } from './src/redux/reducers/themeSlice';
+import store, { persister } from './src/redux/store';
 import {
   darkTheme as BananaDark,
   lightTheme as BananaLight,
-} from './themes/Banana';
+} from './src/themes/Banana';
 import {
   darkTheme as BlueDark,
   lightTheme as BlueLight,
-} from './themes/BlueSapphire';
+} from './src/themes/BlueSapphire';
 import {
   darkTheme as GrapeDark,
   lightTheme as GrapeLight,
-} from './themes/GrapeSoda';
+} from './src/themes/GrapeSoda';
 import {
   darkTheme as GreenDark,
   lightTheme as GreenLight,
-} from './themes/GreenApple';
+} from './src/themes/GreenApple';
 const App = () => {
   const theme = useTheme();
 
   const [fontsLoaded] = useFonts({
-    'Inter-Black': require('../assets/fonts/Inter-Black.ttf'),
-    'Inter-Bold': require('../assets/fonts/Inter-Bold.ttf'),
-    'Inter-Regular': require('../assets/fonts/Inter-Regular.ttf'),
-    'Inter-Bold': require('../assets/fonts/Inter-Bold.ttf'),
-    'Inter-ExtraBold': require('../assets/fonts/Inter-ExtraBold.ttf'),
-    'Inter-ExtraLight': require('../assets/fonts/Inter-ExtraLight.ttf'),
-    'Inter-Light': require('../assets/fonts/Inter-Light.ttf'),
-    'Inter-Medium': require('../assets/fonts/Inter-Medium.ttf'),
-    'Inter-SemiBold': require('../assets/fonts/Inter-SemiBold.ttf'),
-    'Inter-Thin': require('../assets/fonts/Inter-Thin.ttf'),
+    'Inter-Black': require('./assets/fonts/Inter-Black.ttf'),
+    'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+    'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
+    'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+    'Inter-ExtraBold': require('./assets/fonts/Inter-ExtraBold.ttf'),
+    'Inter-ExtraLight': require('./assets/fonts/Inter-ExtraLight.ttf'),
+    'Inter-Light': require('./assets/fonts/Inter-Light.ttf'),
+    'Inter-Medium': require('./assets/fonts/Inter-Medium.ttf'),
+    'Inter-SemiBold': require('./assets/fonts/Inter-SemiBold.ttf'),
+    'Inter-Thin': require('./assets/fonts/Inter-Thin.ttf'),
   });
 
   if (!fontsLoaded) {
